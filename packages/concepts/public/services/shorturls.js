840e78a120aa1d005f811f187af15b6ab6473b1b
Tom Gabrysiak <tomgabrysiak@gmail.com>
2014-10-10 14:27:18 +0000

    removed old slideshow files

--- packages/concepts/public/services/shorturls.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//Concepts service used for concepts REST endpoint
-angular.module('mean')
-.factory('Shorturls', ['$resource',
-    function ($resource) {
-        return $resource('bitly/:conceptId', { conceptId: '@_id' });
-    }
-]);
--- packages/slideshows/public/services/shorturls.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-//Slideshows service used for slideshows REST endpoint
-angular.module('mean')
-.factory('Shorturls', ['$resource',
-    function ($resource) {
-        return $resource('bitly/:slideshowId', { slideshowId: '@_id' });
-    }
-]);
